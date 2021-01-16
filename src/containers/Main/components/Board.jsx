5a4eb678730e1f9015881b562506daf18a470d42
Adriano <adriano.aluizi@xmobots.com.br>
2021-01-16 11:47:53 -0300

    .

--- src/containers/Main/components/Board.jsx
+++ src/containers/Main/components/Board.jsx
@@ -21,7 +21,7 @@ function Board({ data }) {
         <Card value={4} label="Internados" color="#000" />
       </Grid>
       <Grid item xs={12} md={3}>
-        <Card value={14} label="Óbtios" color="#E95078" />
+        <Card value={14} label="Óbitos" color="#E95078" />
       </Grid>
       <Grid item xs={12} md={3}>
         <Card value={553} label="Recuperados" color="#67C887" />
